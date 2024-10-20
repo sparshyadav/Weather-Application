f3b937c17cade6b369150a0149dd660aa1100321
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-10-20 21:44:14 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 import WeatherCard from '../components/WeatherCard';
 
 function Home({ cities }) {
-  return (
-    <div className="main">
-      <h1>Weather Monitoring</h1>
-      <div className="weather-container">
-        {cities.map((city) => (
-          <Link key={city} to={`/city/${city}`}>
-            <WeatherCard city={city} />
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="main">
+            <h1>Weather Monitoring</h1>
+            <div className="weather-container">
+                {cities.map((city) => (
+                    <Link key={city} to={`/city/${city}`}>
+                        <WeatherCard city={city} />
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Home;
